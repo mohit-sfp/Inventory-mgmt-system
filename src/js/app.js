5d2c402903be872a4231aaaeb7191f8d71cfa335
aman-sfp <aman.spmohali@gmail.com>
2020-12-22 16:37:20 +0530

    Latest Code

--- src/js/app.js
+++ src/js/app.js
@@ -1,18 +1,46 @@
 angular.module("helloWorldApp", ["ngRoute"]).config(function ($routeProvider) {
   $routeProvider
     .when("/", {
+      resolve: {
+        check: function ($location, $rootScope) {
+          if (!$rootScope.loggedIn) {
+            $location.path("/login");
+          }
+        },
+      },
       templateUrl: "views/product.html",
       controller: "productController",
     })
     .when("/login", {
+      resolve: {
+        check: function ($location, $rootScope) {
+          if ($rootScope.loggedIn) {
+            $location.path("/");
+          }
+        },
+      },
       templateUrl: "views/login.html",
       controller: "loginController",
     })
     .when("/createProduct", {
+      resolve: {
+        check: function ($location, $rootScope) {
+          if (!$rootScope.loggedIn) {
+            $location.path("/login");
+          }
+        },
+      },
       templateUrl: "views/createProduct.html",
       controller: "createProductController",
     })
     .when("/viewProduct/:id", {
+      resolve: {
+        check: function ($location, $rootScope) {
+          if (!$rootScope.loggedIn) {
+            $location.path("/login");
+          }
+        },
+      },
       templateUrl: "views/viewData.html",
       controller: "viewDataController",
     })
